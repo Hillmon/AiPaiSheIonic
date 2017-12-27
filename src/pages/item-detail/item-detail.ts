fdc1b5a12f4ffb75518ba0c33adc6c3b0878ad8d
Hillmon <hillmon919@gmail.com>
2017-12-27 22:27:52 +0800

    Switched from List to Card presentation in item master page. Added a "Join Event" button to item detail page.

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { LoadingController } from 'ionic-angular';
+
 import { Items } from '../../providers/providers';
 
 @IonicPage()
@@ -10,9 +12,23 @@ import { Items } from '../../providers/providers';
 })
 export class ItemDetailPage {
   item: any;
+  loadingCtrl: any;
 
-  constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
+  constructor(public navCtrl: NavController, navParams: NavParams, items: Items, loadingCtrl: LoadingController) {
     this.item = navParams.get('item') || items.defaultItem;
+    this.loadingCtrl = loadingCtrl;
+  }
+
+  joinEvent() {
+    this.presentLoading();
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Proceeding...Please wait...",
+      duration: 3000
+    });
+    loader.present();
   }
 
 }
