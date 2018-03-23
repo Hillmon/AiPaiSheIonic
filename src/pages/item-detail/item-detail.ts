8d6bb5768b47f9984ffe816220eaaf58393117ed
Hillmon <hillmon919@gmail.com>
2018-03-23 18:03:17 +0800

    Implementation for AIPAISHE-126: APP user can join an event

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -5,6 +5,7 @@ import {Items} from '../../providers/providers';
 import {HttpClient, HttpParams} from "@angular/common/http";
 
 import {format} from 'date-fns';
+import {User} from "../../providers/user/user";
 
 @IonicPage()
 @Component({
@@ -15,13 +16,20 @@ export class ItemDetailPage {
   item: any;
   eventDateStr: string;
   eventTimeStr: string;
+  loader: any;
+  alreadyJoin: boolean;
+  btnText: string;
+  endpoint: string = "http://35.185.217.124:8080";
+
+  // endpoint: string = "http://localhost:8080";
 
   constructor(public navCtrl: NavController,
               navParams: NavParams,
               items: Items,
               private loadingCtrl: LoadingController,
               public http: HttpClient,
-              public toastCtrl: ToastController) {
+              public toastCtrl: ToastController,
+              public user: User) {
     this.item = navParams.get('item') || items.defaultItem;
 
     for (let param in this.item) {
@@ -33,9 +41,15 @@ export class ItemDetailPage {
       const params = new HttpParams()
         .set('id', this.item['ownerId']);
 
-      this.http.get("http://35.185.217.124:8080/user/get", {params}).subscribe(data => {
+      this.http.get(this.endpoint + "/user/get", {params}).subscribe(data => {
+
+          if (data)
 
           this.item['ownerName'] = data['lastName'] + ' ' + data['firstName'];
+
+          else
+
+            this.item['ownerName'] = 'Unknown User';
 
         },
         err => {
@@ -53,9 +67,10 @@ export class ItemDetailPage {
         .set('eventId', this.item['eventId'])
         .set('fileType', 'poster');
 
-      this.http.get("http://35.185.217.124:8080/file/load", {params}).subscribe(data => {
+      this.http.get(this.endpoint + "/file/load", {params}).subscribe(data => {
 
-          console.log('Return Data: ' + data);
+          console.log('Return Data: ');
+          console.log(data);
           if (data[0]) {
             this.item['profilePic'] = data[0]['location'];
           }
@@ -74,16 +89,49 @@ export class ItemDetailPage {
     this.eventTimeStr = format(date, 'HH:MM');
   }
 
-  joinEvent() {
-    this.presentLoading();
+  ionViewDidLoad() {
+    // Put here the code you want to execute
+    this.checkEvent();
   }
 
-  presentLoading() {
-    let loader = this.loadingCtrl.create({
-      content: "Proceeding...Please wait...",
+  joinEvent() {
+
+    this.presentLoading("Joining the event for you...");
+
+    // retrieve the login user profile with the user service
+    let userProfile = this.user.getLoginUser();
+
+    // retrieve the event profile pic by the event ID
+    if (this.item['eventId'] && userProfile['id']) {
+      const params = new HttpParams()
+        .set('eventId', this.item['eventId'])
+        .set('userId', userProfile['id']);
+
+      this.http.get(this.endpoint + "/eulink/create", {params}).subscribe(data => {
+
+          console.log('Return Data: ');
+          console.log(data);
+          if (data[0]) {
+            // this.item['profilePic'] = data[0]['location'];
+          }
+
+        },
+        err => {
+          this.presentToast("Create event-user link error!");
+          console.error(err);
+        });
+    }
+    else {
+      this.presentToast('Event ID or User ID not found!');
+    }
+  }
+
+  presentLoading(msg) {
+    this.loader = this.loadingCtrl.create({
+      content: msg,
       duration: 3000
     });
-    loader.present();
+    this.loader.present();
   }
 
   presentToast(msg) {
@@ -98,6 +146,48 @@ export class ItemDetailPage {
     });
 
     toast.present();
+  }
+
+  checkEvent(): any {
+    this.presentLoading("Checking the event status...");
+
+    // retrieve the login user profile with the user service
+    let userProfile = this.user.getLoginUser();
+
+    console.log("Current event ID:" + this.item['eventId']);
+
+    console.log("Current login user profile:");
+    console.log(userProfile);
+
+    // retrieve the event profile pic by the event ID
+    if (this.item['eventId'] && userProfile['id']) {
+      console.log("Ready to check the status...");
+
+      const params = new HttpParams()
+        .set('eventId', this.item['eventId'])
+        .set('userId', userProfile['id']);
+
+      this.http.get(this.endpoint + "/eulink/check", {params}).subscribe(data => {
+
+          this.loader.dismissAll();
+
+          if (data) {
+            this.alreadyJoin = true;
+            this.btnText = 'You are already in!'
+          }
+          else {
+            this.alreadyJoin = false;
+            this.btnText = 'Join this event!';
+          }
+        },
+        err => {
+          this.presentToast("Checking event-user link error!");
+          console.error(err);
+        });
+    }
+    else {
+      this.presentToast('Event ID or User ID not found!');
+    }
   }
 
 }
